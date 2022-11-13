9a20142cc5bc7dff2bd7cdf568cbe1b568b85929
Carlos Lemos <32042584+clabemo@users.noreply.github.com>
2022-11-13 00:27:50 -0300

    Update openscap_scan.sh

--- openscap_scan.sh
+++ openscap_scan.sh
@@ -3,7 +3,7 @@
 DATE=$(date +%m%d%Y)
 
 # Download oval file
-wget https://www.debian.org/security/oval/oval-definitions-bullseye.xml
+wget -O https://www.debian.org/security/oval/oval-definitions-bullseye.xml /var/tmp/oval-definitions-bullseye.xml
 
 # Scan for vulnerability
 oscap oval eval --results /var/tmp/report_${DATE}.xml --report /var/tmp/report_${DATE}.html /var/tmp/oval-definitions-bullseye.xml
